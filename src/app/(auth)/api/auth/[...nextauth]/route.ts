a68e94f1e4d44a157a5a0f9d9b99d4040584e540
unknown <oguntayomicheal@gmail.com>
2024-08-16 12:09:07 +0100

    Add nextauth api route

---
+++ src/app/(auth)/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,39 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import { PrismaClient } from "@prisma/client"
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+ import prisma from "@/lib/prismadb";
+
+ import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+
+  debug: process.env.NODE_ENV === 'development', 
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+const handler = NextAuth(authOptions)
+
+export {handler as GET, handler as POST}
+
+// const prisma = new PrismaClient()
+ 
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//    providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+// })
