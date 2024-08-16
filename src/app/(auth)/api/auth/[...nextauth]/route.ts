3d647bcf4a9f3e943466848138f21d4447429856
unknown <oguntayomicheal@gmail.com>
2024-08-16 21:33:13 +0100

    Update backend without sensitive data

--- src/app/(auth)/api/auth/[...nextauth]/route.ts
+++ src/app/(auth)/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,38 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 // import { PrismaAdapter } from "@auth/prisma-adapter";
- import prisma from "@/lib/prismadb";
+import prisma from "@/lib/prismadb";
 
- import GoogleProvider from "next-auth/providers/google";
-
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
-  
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
+    })
   ],
 
-  debug: process.env.NODE_ENV === 'development', 
-  secret: process.env.NEXTAUTH_SECRET
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
 
 // const prisma = new PrismaClient()
- 
+
 // export default NextAuth({
 //   adapter: PrismaAdapter(prisma),
 //    providers: [
