a8eb4ee3ea51dd6b44b9b17337da83e1e62668f3
unknown <oguntayomicheal@gmail.com>
2024-08-18 22:34:37 +0100

    Update userposts page

--- src/app/(auth)/api/auth/[...nextauth]/route.ts
+++ src/app/(auth)/api/auth/[...nextauth]/route.ts
@@ -17,10 +17,10 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
 
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID as string,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-    })
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
+    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
+    // })
   ],
 
   debug: process.env.NODE_ENV === "development",
@@ -30,15 +30,3 @@ export const authOptions: AuthOptions = {
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-
-// const prisma = new PrismaClient()
-
-// export default NextAuth({
-//   adapter: PrismaAdapter(prisma),
-//    providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID as string,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//     }),
-//   ],
-// })
