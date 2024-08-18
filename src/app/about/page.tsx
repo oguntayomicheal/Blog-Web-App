52bcf5e97553856e9fb882786a199e80d4c488f0
unknown <oguntayomicheal@gmail.com>
2024-08-19 00:38:40 +0100

    Update TopPost page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,18 +29,13 @@ const page = () => {
       "
       >
         <p>
-          ddddd "leading-8 text-lg bg-white mt-[-80px] relative w-[90%] m-auto
-          rounded-lg p-5 shadow-xl text-center max-md:bg-transparent max-md:mt-0
-          max-md:w-full max-md:shadow-none "
-          ddddd "leading-8 text-lg bg-white mt-[-80px] relative w-[90%] m-auto
-          rounded-lg p-5 shadow-xl text-center max-md:bg-transparent max-md:mt-0
-          max-md:w-full max-md:shadow-none "
-          ddddd "leading-8 text-lg bg-white mt-[-80px] relative w-[90%] m-auto
-          rounded-lg p-5 shadow-xl text-center max-md:bg-transparent max-md:mt-0
-          max-md:w-full max-md:shadow-none "
-          ddddd "leading-8 text-lg bg-white mt-[-80px] relative w-[90%] m-auto
-          rounded-lg p-5 shadow-xl text-center max-md:bg-transparent max-md:mt-0
-          max-md:w-full max-md:shadow-none "
+          Welcome to Explore X, your ultimate companion for adventure and discovery! 
+          Explore X is more than just a blog app—it's a vibrant community for those who
+           live with curiosity and a passion for the world around them. Whether you're 
+           an avid traveler, a local explorer, or someone who loves to dive deep into 
+           new cultures and ideas, Explore X offers a platform to share your stories, insights, and experiences.
+          Our mission is to inspire and connect like-minded adventurers who seek to 
+          explore the extraordinary. From hidden gems in your hometown to far-flung destinations across the globe, Explore X is where your journey begins. Join us as we celebrate the beauty of exploration, one story at a time.
         </p>
 
 
